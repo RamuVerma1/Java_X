36024c3d474944728b56d75dd9e5bdf9c4929244
RamuVerma1 <rosuperhit.2002@gmail.com>
2024-12-24 23:35:14 +0530

    first commit

---
+++ project_01/src/first.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class first {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);  //scanner sc = new Scanner(System.in); 
+    // Scanner sc = new Scanner(System.in) ; 
+    //int age =sc.nextInt(); 
+System.out.println("enter the number too printt");
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+             System.out.print("* ");
+        }
+        System.out.print("\n");
+       
+    }
+    sc.close();
+
+
+
+
+    }
+
+}
